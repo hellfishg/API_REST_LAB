b83f324cb613741b76f5707c0bea1b2faddd3c1a
hellfishg <hellfish.g@protonmail.com>
2021-09-22 20:33:26 -0300

    Configuracion SQL_Postgres

---
+++ src/main/java/aero/api_rest/ApiRestApplication.java
@@ -0,0 +1,14 @@
+package aero.api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestApplication.class, args);
+		System.out.println("hello word!");
+	}
+
+}
