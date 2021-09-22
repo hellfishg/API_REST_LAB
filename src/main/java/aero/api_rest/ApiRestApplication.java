c528bd8b9be4d56c5d3d048722d1283cd38e8f94
hellfishg <hellfish.g@protonmail.com>
2021-09-22 20:55:52 -0300

    notacion para autoconfiguracion de DB

--- src/main/java/aero/api_rest/ApiRestApplication.java
+++ src/main/java/aero/api_rest/ApiRestApplication.java
@@ -1,8 +1,11 @@
 package aero.api_rest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class ApiRestApplication {
 
